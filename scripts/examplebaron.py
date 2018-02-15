8ce5c2ca966026764eea595e98672ff98e64a93b
David Bernal <bernalde@cmu.edu>
2018-02-15 01:39:30 -0500

    changes to the baron script to work in general

--- scripts/examplebaron.py
+++ scripts/examplebaron.py
@@ -10,16 +10,16 @@ from cStringIO import StringIO
 
 codefiles = ['fac1.py','portfol_buyin.py','portfol_card.py']
 timetable = [['Instance', 'Time Elapsed']]
-
-os.chdir('/home/felicitg/MINLPinstances/py')
+fileDir = os.path.dirname(os.path.realpath('__file__'))
+fileDir = os.path.join(fileDir, '../py')
+os.chdir(fileDir)
 sys.path.append(os.getcwd())
-
 for filename in sorted(os.listdir('.')):
     # Only do things with .py files
-    if filename.endswith('.py') and filename not in codefiles:
+    if filename.endswith('.py') and filename in codefiles:
         name = os.path.splitext(filename)[0]
         print(name) 
-        newname = name + 'baron.txt'
+        newname = name + '_baron.txt'
         f = open(newname, "w+")
         f.write(filename + '\n')
         old_stdout = sys.stdout
@@ -27,16 +27,16 @@ for filename in sorted(os.listdir('.')):
         filename1 = __import__(name)
         start = time.time()
         opt = SolverFactory('baron')
-        opt.solve(filename1.m, timelimit=900, tee=True)
+        # opt.solve(filename1.m, timelimit=900, tee=True)
         end = time.time()
         sys.stdout = old_stdout
         f.write(mystdout.getvalue())
         f.write("\nTime elapsed: {}".format(end-start))
         f.close()
         timetable.append([filename, end-start])
-        subprocess.call(['mv '+ newname + ' ../oaresults'],shell=True)
+        subprocess.call(['mv '+ newname + ' ../baronresults'],shell=True)
 
-subprocess.call(['cd oaresults'])
 with open('1-instancetablebaron.csv', 'w+') as table:
     writer = csv.writer(table)
     [writer.writerow(r) for r in timetable]
+subprocess.call(['mv 1-instancetablebaron.csv ../baronresults'],shell=True)
